1871371a240161318e6bba62c15f4ce5383bb3d2
honza <jena.fruhauf@protonmail.com>
2026-09-15 08:54:20 +0200

    oprava robota

--- 03Robot/robot.py
+++ 03Robot/robot.py
@@ -1,7 +1,28 @@
-class Hero:
+class Robot:
     def __init__(self, oznaceni: str, baterie: int, ukol: str):
         self.oznaceni = oznaceni
         self.baterie = baterie
         self.ukol = ukol
 
-    
+    def zvuk(self):
+        return "BípPup"
+
+    def diagnositka(self):
+        return f"{self.baterie}, {self.oznaceni}"
+
+    def aktualniUkol(self):
+        return self.ukol
+
+    def zadejUkol(self, nUkol: str):
+        self.ukol = nUkol
+        return f"tvuj nový úkol je {self.ukol} "
+
+
+robot = Robot("BíPuBíp", 80, "umýt nádobí")
+print(robot.oznaceni)
+print(robot.baterie)
+print(robot.ukol)
+print(robot.zvuk())
+print(robot.diagnositka())
+print(robot.aktualniUkol())
+print(robot.zadejUkol("uklidit nádobí"))
