7c6a8209cfcc4b906af3ff9b0be7cd811960d09c
honza <jena.fruhauf@protonmail.com>
2026-09-15 08:30:34 +0200

    ukol splnen hero

--- 02Hrdina/hero.py
+++ 02Hrdina/hero.py
@@ -1,5 +1,5 @@
 class Hero:
-    def __init__(self, jmeno: str, level: int, lokace: str = "Valorant"):
+    def __init__(self, jmeno: str, level: int, lokace: str = "site B"):
         self.jmeno = jmeno
         self.level = level
         self.lokace = lokace
@@ -10,9 +10,20 @@ class Hero:
     def predstavSe(self):
         return f"Jmenuji se {self.jmeno}, a mám {self.level} level."
 
+    def KdeJsi(self):
+        return f"jsem v Lokaci zvaném {self.lokace}."
+
+    def PresunSe(self, nLokace: str = "site A"):
+        staraLokace = self.lokace
+        self.lokace = nLokace
+        return f"přesouvám se z lokace {staraLokace} na {self.lokace}"
+
 
 hero = Hero("Láďa", 27)
 print(hero.jmeno)
 print(hero.level)
 print(hero.lokace)
+print(hero.pokrik())
 print(hero.predstavSe())
+print(hero.KdeJsi())
+print(hero.PresunSe())
